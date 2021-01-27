bd143dc063925968cd43dbb5b24c1543f026c4c2
conclaveRose <conclaveyohan.l@gmail.com>
2021-01-28 02:05:41 +0900

    second commit

--- main.js
+++ main.js
@@ -3,9 +3,12 @@
 //selector
 const zone = document.querySelector('.zone');
 const container = document.querySelector('#container');
+const leftI = document.querySelector('.fas.fa-angle-left');
+const rightI = document.querySelector('.fas.fa-angle-right');
 
 //variable
 const leftBtn = "fas fa-angle-left";
+const rightBtn = "fas fa-angle-right";
 
 // addEventListener
 document.addEventListener('DOMContentLoaded', classDefault);
@@ -25,14 +28,33 @@ function DoSlide(event) {
             case 'default':
                 container.classList.remove('default');
                 container.classList.add('left');
+                rightI.style.display = 'block';
                 break;
             case 'left':
                 container.classList.remove('left');
                 container.classList.add('left2');
+                leftI.style.display = 'none';
+                rightI.style.display = 'block';
+                break;
+        }
+
+    } else if (btn.classList == rightBtn) {
+        switch (container.className) {
+            case 'left2':
+                container.classList.remove('left2');
+                container.classList.add('left');
+                leftI.style.display = 'block';
+                break;
+            case 'left':
+                container.classList.remove('left');
+                container.classList.add('default');
+                rightI.style.display = 'none';
                 break;
         }
     }
-}
+};
+
+
 
 
 
